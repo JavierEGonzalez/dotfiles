4bb07fffe6154538183cac48c4ef98d0c8ae4664
JavierEGonzalez <javier.gonzalez1@adusa.com>
2025-01-03 10:34:23 -0600

    add ticket stuff to gct

---
+++ scripts/git_commit_session_ticket.sh
@@ -0,0 +1,40 @@
+#/usr/local/bin/bash
+
+if [[ -z $ticket ]]; then
+  echo "Ticket is not set"
+  echo "Setup session ticket:"
+  read -p ">" ticket
+  export ticket=$ticket && tmux setenv ticket $ticket
+fi
+
+if [[ $(git status | tail -n 1) == "nothing to commit, working tree clean" ]]; then
+  echo "No changes to commit"
+  exit 1
+fi
+if [[ $(git status | tail -n 1 | sed -e "s/ (.*//g") == "no changes added to commit" ]]; then
+  echo "No changes added to commit, run git add .?"
+  select shouldAdd in "yes" "cancel"; do
+    case $shouldAdd in
+    yes)
+      git add .
+      break
+      ;;
+    cancel)
+      exit 1
+      ;;
+    esac
+  done
+fi
+
+echo "Committing to $ticket"
+
+if [[ -z $1 ]]; then
+  echo "Message:"
+  read -p ">" msg
+else
+  msg=$1
+  echo "Message: '$msg'"
+fi
+
+echo "RUNNING THE FOLLOWING COMMAND: git commit -m '$ticket: $msg' --no-verify"
+git commit -m "$ticket: $msg" --no-verify
