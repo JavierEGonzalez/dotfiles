2be5f18e7e76ed5439c6c8dedfdeaf4b5e0f81c4
JavierEGonzalez <javier.gonzalez1@adusa.com>
2025-07-31 09:05:21 -0500

    ai stuff

--- scripts/workers_ai.sh
+++ scripts/workers_ai.sh
@@ -14,15 +14,25 @@ else
   SYSTEM_PROMPT=$(<~/.scratch/scripts/workers_ai_bash_assistant_prompt)
 fi
 
-curl -s \
-  "https://api.cloudflare.com/client/v4/accounts/$ACCOUNT_ID/ai/run/@cf/meta/llama-3-8b-instruct" \
+if [ "$USE_DEFAULT_BASH_ASSISTANT_PROMPT" = true ]; then
+  MAX_TOKENS=512
+else
+  MAX_TOKENS=2048
+fi
+
+curl  -N -s \
+  "https://api.cloudflare.com/client/v4/accounts/$ACCOUNT_ID/ai/run/@cf/meta/llama-4-scout-17b-16e-instruct" \
   -H "Authorization: Bearer $API_TOKEN" \
   -H "Content-Type: application/json" \
-  -d "$(jq -n --arg sys "$SYSTEM_PROMPT" --arg user "$USER_MESSAGE" \
-    '{messages: [{role: "system", content: $sys}, {role: "user", content: $user}]}')" \
+  -d "$(jq -n --arg sys "$SYSTEM_PROMPT" --arg user "$USER_MESSAGE" --argjson tokens $MAX_TOKENS \
+    '{messages: [{role: "system", content: $sys}, {role: "user", content: $user}], max_tokens: $tokens }')" \
   | jq -r '.result.response' > /tmp/workers_ai_response.sh
 
-bat /tmp/workers_ai_response.sh -l zsh -n
+if [ "$USE_DEFAULT_BASH_ASSISTANT_PROMPT" = true ]; then
+  bat /tmp/workers_ai_response.sh -l zsh -n
+else
+  bat /tmp/workers_ai_response.sh -l md -n
+fi
 
 if [ "$USE_DEFAULT_BASH_ASSISTANT_PROMPT" = true ]; then
   echo "Do you want to execute the command? (y/n)"
