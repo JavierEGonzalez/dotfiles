a630c6a4b145c995a16b870fd58eed34f40c678c
JavierEGonzalez <javier.gonzalez1@adusa.com>
2025-09-16 12:12:10 -0500

    updates

--- .zshrc
+++ .zshrc
@@ -61,11 +61,11 @@ source $ZSH/oh-my-zsh.sh
 # export LANG=en_US.UTF-8
 
 # Preferred editor for local and remote sessions
-# if [[ -n $SSH_CONNECTION ]]; then
-#   export EDITOR='vim'
-# else
-#   export EDITOR='nvim'
-# fi
+if [[ -n $SSH_CONNECTION ]]; then
+  export EDITOR='vim'
+else
+  export EDITOR='nvim'
+fi
 
 # Compilation flags
 # export ARCHFLAGS="-arch $(uname -m)"
@@ -112,4 +112,5 @@ if command -v direnv >/dev/null 2>&1; then
   eval "$(direnv hook zsh)"
 fi
 
-fpath+=~/.config/zellij/zellij_zsh_completions.zsh
+
+export PATH="$HOME/.local/bin:$PATH"
--- scripts/adusa/git_commit_session_ticket.sh
+++ scripts/adusa/git_commit_session_ticket.sh
@@ -3,9 +3,19 @@ source ~/.scratch/scripts/session_ticket_functions.sh
 
 if [[ -z $ticket || $ticket == 'CXPVSP-' ]]; then
   echo "Ticket is not set"
-  echo "Setup session ticket:"
-  read -p ">" ticket
-  export ticket=$ticket && tmux setenv ticket $ticket
+  # check if ticket is set in branch name
+  branch=$(git rev-parse --abbrev-ref HEAD)
+  if [[ $branch =~ (CXPVSP-[0-9]+) ]]; then
+    ticket=${BASH_REMATCH[1]}
+    echo "Found ticket in branch name: $ticket"
+    export ticket=$ticket 
+    #redirect to dev/null if there's error
+    tmux setenv ticket $ticket 2>/dev/null
+  else
+    echo "Setup session ticket:"
+    read -p ">" ticket
+    export ticket=$ticket && tmux setenv ticket $ticket
+  fi
 fi
 
 prompt_to_stage_if_needed
